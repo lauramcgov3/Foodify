fa588e00b64d538b0fad5b93884e101a2c2bfcce
Laura <laura.mcgovern3@gmail.com>
2018-11-25 14:22:31 +0000

    Refactoring URLs and extending HTML

--- foodifyapp/views.py
+++ foodifyapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from .models import DaysOfTheWeek
 
-def days_list(request):
+def home(request):
     days = DaysOfTheWeek.objects.all().order_by('pk')
-    return render(request, 'foodifyapp/days_list.html', {'days':days})
+    return render(request, 'foodifyapp/home.html', {'days':days})
